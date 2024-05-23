3627fa8e794ba8dd90ab2bc42e9b815f7ecba788
miguelalves10 <miguelmiguelpires@gmail.com>
2024-05-23 20:54:30 -0300

    Calcular o somar e função chamando função

---
+++ js/inicio/script2.js
@@ -0,0 +1,25 @@
+/* 
+Objetivo: calcular a média de 2 números
+    1- Pegar os 2 números
+    2- Somar os 2 números
+    3- Dividir a soma pela quantidade de números
+*/
+
+
+let n1 = 100;
+let n2 = 900;
+
+function soma() {
+    console.log(n1 + n2);
+    return n1+n2
+}
+
+soma()
+
+
+function media() {
+    console.log(soma()/ 2);
+    
+}
+
+media()
--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('script via body')
+// console.log('script via body', document.getElementById('n1'))
+
+function soma() {
+    // Pegar os números dos inputs do HTML e converter para Float, pois será possível o cálculo inteiro
+    // e decimais
+    let numero1 = parseFloat(document.getElementById('n1').value);
+    let numero2 = parseFloat(document.getElementById('n2').value);
+
+    let resultado = numero1 + numero2;
+    
+    console.log(resultado);
+}
