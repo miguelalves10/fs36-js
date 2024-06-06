ce3cd8823fe3090fb84265996bde0066a5dc54bb
miguelalves10 <miguelmiguelpires@gmail.com>
2024-06-06 20:02:04 -0300

    alterações

--- js/script.js
+++ js/script.js
@@ -1,12 +1,52 @@
 // console.log('script via body', document.getElementById('n1'))
 
-function soma() {
+function somar() {
     // Pegar os números dos inputs do HTML e converter para Float, pois será possível o cálculo inteiro
     // e decimais
     let numero1 = parseFloat(document.getElementById('n1').value);
     let numero2 = parseFloat(document.getElementById('n2').value);
-
-    let resultado = numero1 + numero2;
+    let resultado_somar = numero1 + numero2;
     
-    console.log(resultado);
+
+    console.log(resultado_somar);
+
+
+    // exibindo o resultado da div resultado através da prop innerHTML
+    let conteudo_da_div = document.getElementsByClassName('resultado_somar')[0];
+    conteudo_da_div.innerHTML = resultado_somar
+}
+
+function subtrair() {
+    let numero1 = parseFloat(document.getElementById('n1').value);
+    let numero2 = parseFloat(document.getElementById('n2').value);
+    let resultado_subtrair = numero1 - numero2;
+
+    console.log(resultado_subtrair);
+
+    let conteudo_da_div = document.getElementsByClassName('resultado_subtrair')[0];
+    conteudo_da_div.innerHTML = resultado_subtrair
+}
+
+function multiplicar() {
+    let numero1 = parseFloat(document.getElementById('n1').value);
+    let numero2 = parseFloat(document.getElementById('n2').value);
+    let resultado_multiplicar = numero1 * numero2;
+
+
+    console.log(resultado_multiplicar);
+
+    let conteudo_da_div = document.getElementsByClassName('resultado_multiplicar')[0];
+    conteudo_da_div.innerHTML = resultado_multiplicar
+}
+
+function dividir() {
+    let numero1 = parseFloat(document.getElementsByClassName('n1').value);
+    let numero2 = parseFloat(document.getElementsByClassName('n2').value);
+    resultado_dividir = numero1/numero2;
+
+
+    console.log(resultado_dividir);
+
+    let conteudo_da_div = document.getElementsByClassName('resultado_dividir')[0];
+    conteudo_da_div.innerHTML = resultado_dividir
 }
