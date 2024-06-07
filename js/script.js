44722e090abb787523aac56685a1145a991d6939
miguelalves10 <miguelmiguelpires@gmail.com>
2024-06-06 21:49:39 -0300

    alterações

--- js/script.js
+++ js/script.js
@@ -1,52 +1,37 @@
-// console.log('script via body', document.getElementById('n1'))
+const numero1 = (document.getElementById('n1'));
+const numero2 = (document.getElementById('n2'));
+const conteudo_da_div = document.querySelector('.resultado')
 
 function somar() {
-    // Pegar os números dos inputs do HTML e converter para Float, pois será possível o cálculo inteiro
-    // e decimais
-    let numero1 = parseFloat(document.getElementById('n1').value);
-    let numero2 = parseFloat(document.getElementById('n2').value);
-    let resultado_somar = numero1 + numero2;
-    
-
-    console.log(resultado_somar);
-
-
-    // exibindo o resultado da div resultado através da prop innerHTML
-    let conteudo_da_div = document.getElementsByClassName('resultado_somar')[0];
-    conteudo_da_div.innerHTML = resultado_somar
+    let resultado = parseFloat(numero1.value) + parseFloat(numero2.value);
+    mostrar_resultado(resultado)
 }
 
 function subtrair() {
-    let numero1 = parseFloat(document.getElementById('n1').value);
-    let numero2 = parseFloat(document.getElementById('n2').value);
-    let resultado_subtrair = numero1 - numero2;
-
-    console.log(resultado_subtrair);
-
-    let conteudo_da_div = document.getElementsByClassName('resultado_subtrair')[0];
-    conteudo_da_div.innerHTML = resultado_subtrair
+    let resultado = parseFloat(numero1.value) - parseFloat(numero2.value);
+    mostrar_resultado(resultado)
 }
 
 function multiplicar() {
-    let numero1 = parseFloat(document.getElementById('n1').value);
-    let numero2 = parseFloat(document.getElementById('n2').value);
-    let resultado_multiplicar = numero1 * numero2;
-
-
-    console.log(resultado_multiplicar);
-
-    let conteudo_da_div = document.getElementsByClassName('resultado_multiplicar')[0];
-    conteudo_da_div.innerHTML = resultado_multiplicar
+    let resultado = parseFloat(numero1.value) * parseFloat(numero2.value);
+    mostrar_resultado(resultado)
 }
 
 function dividir() {
-    let numero1 = parseFloat(document.getElementsByClassName('n1').value);
-    let numero2 = parseFloat(document.getElementsByClassName('n2').value);
-    resultado_dividir = numero1/numero2;
+    let resultado = parseFloat(numero1.value) / parseFloat(numero2.value);
+    mostrar_resultado(resultado)
+}
 
+function mostrar_resultado(resultado_parametro) {
+    conteudo_da_div.innerHTML = 'Resultado: ' + resultado_parametro
 
-    console.log(resultado_dividir);
+}
 
-    let conteudo_da_div = document.getElementsByClassName('resultado_dividir')[0];
-    conteudo_da_div.innerHTML = resultado_dividir
+function limpar() {
+    conteudo_da_div.style.display = 'none'
+// codigo para limpar o que digitei
+    // numero1.value = '';
+    // numero2.value = '';
+    // conteudo_da_div.innerHTML = '';
+    conteudo_da_div.innerHTML = 'Resultado: ';
 }
